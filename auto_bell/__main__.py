e8ed36ed666e00209ac8d8a6a2360117099949a1
Stefannus Christian <schristian38@students.calvin.ac.id>
2024-01-12 08:16:11 +0700

    smk kimia industri theresiana v1

---
+++ auto_bell/__main__.py
@@ -0,0 +1,6 @@
+from bell_automation import BellAutomation
+
+if __name__ == "__main__":
+    # Create an instance of BellAutomation and run the automation
+    bell = BellAutomation()
+    bell.run()
